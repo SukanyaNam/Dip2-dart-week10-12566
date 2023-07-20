ea653b74daa377cb7362d8948081e21b3cfd33ca
SukanyaNam <kjjj5406@gmail.com>
2023-07-20 15:49:38 +0700

    first commit

---
+++ bin/dratweek10.dart
@@ -0,0 +1,10 @@
+import 'package:dratweek10/condition.dart';
+import 'package:dratweek10/generation.dart';
+
+void main() {
+  generation();
+  //listIfElseIfDemo();
+  // ifElseIfDemo();
+  // ifElseDemo();
+  // ifDemo();
+}
---
+++ test/dratweek10_test.dart
@@ -0,0 +1,8 @@
+import 'package:dratweek10/dratweek10.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
